4ecf5c396693961a3c79ba589f4d1068dacf6870
Баранов Єгор <kolllapa.exe@gmail.com>
2026-09-09 11:10:22 +0300

    Add files via upload

---
+++ Project-03/script.js
@@ -0,0 +1,63 @@
+// let age = prompt('Enter your age');
+// let access = confirm("Are you registered?");
+//
+// if (age >= 18 && access === true) {
+//     alert("Welcome!")
+// }else{
+//     alert("Access denied!")
+// }
+//
+// let role = prompt("What is your role?");
+// if (role === "admin" || role === "teacher") {
+//     alert('welcome')
+// }else{
+//     alert('lox')
+// }
+
+// let acc = confirm("Are you signed up?");
+// let age = prompt("How old are you?");
+// if (acc === true){
+//     if (age >= 18){
+//         alert("yipeee")
+//     }else{
+//         alert("access denied")
+//     }
+// }else{
+//     alert("access denied")}
+
+// let age = prompt('Enter your age');
+// if (age > 18) {
+//     alert("Old")
+// }else if ( age > 14){
+//     alert("Teen")
+// }else if (age > 6){
+//     alert("school")
+// }else {
+//     alert("Kid");
+// }
+
+// let age = prompt("Age?")
+// if (age >= 18) {
+//     alert("access granted")
+// }else if (age < 18) {
+//     let access = confirm("Access?")
+//     if (access === true) {
+//         alert("access granted")
+//     } else {
+//         alert("lox")
+//     }
+// }
+// const sale1 = 0.1
+// let name = prompt("What is the product name?");
+// let price = prompt("Price?");
+// let quantity = prompt("Quantity?");
+// let sign = confirm("Are you signed in?");
+// let promo = prompt("Have promo?");
+// const promo2 = "sale";
+// let VIP = confirm("status?");
+//
+// if (price*quantity >= 1000 && sign === true && (promo === promo2 || VIP === true)) {
+//     alert(`Price with sale:${price*quantity - price*quantity*sale1} uah`)
+// }else{
+//     alert(`No sale price ${price*quantity} uah`)
+// }
